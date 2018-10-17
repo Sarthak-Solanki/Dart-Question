fe62ff59a702a5ec9f8c64f3ebd4528d766a2474
Sarthak-Solanki <sarthaksolanki08@gmail.com>
2018-10-17 18:54:32 +0530

    17 oct

--- DataStructure/HasingFunction.dart
+++ DataStructure/HasingFunction.dart
@@ -6,5 +6,13 @@ class HashingFunction{
  List l = new List();
  l = st.split(' ');
  print(l);
+ int m = 10;
+ int n = 10;
+  var x = new List.generate(m, (_)=> new List(n));
+for(int i = 0;i<=st.length;i++){
+  int z = 0;
+  z = l[i]%11;
+  l
 }
+  }
 }
